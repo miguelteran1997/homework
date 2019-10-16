7dde5ae948d11627412f7f5c1a78d4af1658c270
miguelteran1997 <54339141+miguelteran1997@users.noreply.github.com>
2019-10-16 11:27:11 -0500

    Add files via upload

---
+++ Coaches/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using OffPractice.Data;
+
+namespace OffPractice.Pages.Coaches
+{
+    public class IndexModel : PageModel
+    {
+        private readonly OffPractice.Data.ApplicationDbContext _context;
+
+        public IndexModel(OffPractice.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Coach> Coach { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Coach = await _context.Coach.ToListAsync();
+        }
+    }
+}
